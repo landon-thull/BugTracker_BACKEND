41c6b8d4d2c8f562ef2c478e0e702b07967b1284
Landon Thull <98464797+landon-thull@users.noreply.github.com>
2022-11-17 16:03:07 -0600

    added auth

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,18 +16,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Public Routes
+//Auth Routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
+
+//Projects Routes
 Route::get('/projects', [ProjectController::class, 'index']);
-
-Route::post('/projects', [ProjectController::class, 'store']);
-
 Route::get('/projects/{id}', [ProjectController::class, 'show']);
-
-Route::put('/projects/{id}', [ProjectController::class, 'update']);
-
-Route::delete('/projects/{id}', [ProjectController::class, 'destroy']);
-
 Route::get('/projects/search/{name}', [ProjectController::class, 'search']);
+
+
+// Private Routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    // Aruth Routes
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Projects Routes
+    Route::post('/projects', [ProjectController::class, 'store']);
+    Route::put('/projects/{id}', [ProjectController::class, 'update']);
+    Route::delete('/projects/{id}', [ProjectController::class, 'destroy']);
+});
